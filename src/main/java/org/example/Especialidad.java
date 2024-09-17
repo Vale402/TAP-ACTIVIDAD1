15fc0c269664c1364400ab61a5a7643cf4919297
merafe26MRF <melanyefernandez200@gmail.com>
2024-09-16 19:59:47 -0600

    Agregue metodos getters y setters a la clase Especialidad

--- src/main/java/org/example/Especialidad.java
+++ src/main/java/org/example/Especialidad.java
@@ -14,13 +14,35 @@ public class Especialidad {
     public static void agregarEspecialidad(Especialidad especialidad) {
         listaEspecialidades[contador++] = especialidad;
     }
-
     public String getNombre() {
         return nombre;
     }
 
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
     public String getClave() {
         return clave;
+    }
+    public void setclave(String clave){
+        this.clave = clave;
+    }
+
+    public static Especialidad[] getListaEspecialidades() {
+        return listaEspecialidades;
+    }
+
+    public static void setListaEspecialidades(Especialidad[] listaEspecialidades) {
+        Especialidad.listaEspecialidades = listaEspecialidades;
+    }
+
+    public static int getContador() {
+        return contador;
+    }
+
+    public static void setContador(int contador) {
+        Especialidad.contador = contador;
     }
 
     public static void mostrarEspecialidades() {
