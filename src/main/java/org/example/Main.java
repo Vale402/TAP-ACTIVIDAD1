ed9bf45118c418d46ca308442e0937b9eb323a78
Valeria <valeriaponce402@gmail.com>
2024-09-16 21:51:12 -0600

    Arreglo errores

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,6 @@
 package org.example;
 import java.util.Scanner;
 
-import static org.example.Alumno.agregarAlumno;
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
